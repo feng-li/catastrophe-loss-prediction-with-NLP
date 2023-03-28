167b04e52fbf31ef6ce737613fe1490f8869312c
wwwenwang <113757503+wwwenwang@users.noreply.github.com>
2023-03-28 15:22:47 +0800

    Create xgb_reg.py

---
+++ catastrophe-duration-and-loss-prediction/model-for-duration-prediction/xgb_reg.py
@@ -0,0 +1,10 @@
+def xgb_reg(trial):
+    params = {'objective': 'reg:squarederror', 'random_state': 0, 
+              'max_depth': trial.suggest_int('max_depth', 3, 10), 
+              'learning_rate': trial.suggest_float('learning_rate', 0.001, 1), 
+              'reg_alpha': trial.suggest_float('reg_alpha', 0, 1.0), 
+              'reg_lambda': trial.suggest_float('reg_lambda', 0, 1.0), }
+    model = xgb.XGBRegressor(**params)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    return np.sqrt(sum((y_test - y_pred)**2) / len(y_test))
