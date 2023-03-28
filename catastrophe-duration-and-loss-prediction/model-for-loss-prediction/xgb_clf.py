344e82a8ad5826bf1a2f6292aeef39d68cf3458a
wwwenwang <113757503+wwwenwang@users.noreply.github.com>
2023-03-28 15:25:48 +0800

    Create xgb_clf.py

---
+++ catastrophe-duration-and-loss-prediction/model-for-loss-prediction/xgb_clf.py
@@ -0,0 +1,10 @@
+def xgb_clf(trial):
+    params = {'objective': 'binary:logistic', 'random_state': 0, 
+              'max_depth': trial.suggest_int('max_depth', 3, 10), 
+              'learning_rate': trial.suggest_float('learning_rate', 0.001, 1), 
+              'reg_alpha': trial.suggest_float('reg_alpha', 0, 1.0), 
+              'reg_lambda': trial.suggest_float('reg_lambda', 0, 1.0), }
+    model = xgb.XGBClassifier(**params)
+    model.fit(x_train, y_label_train)
+    y_pred = model.predict(x_test)
+    return sum(y_pred == y_label_test) / len(y_label_test)
